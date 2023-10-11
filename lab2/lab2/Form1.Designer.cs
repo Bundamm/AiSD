ec4e1dee05eca6b26abe09a2a5cff1a49029c34a
Tomasz Szewa <tomcio.szewa@gmail.com>
2023-10-11 09:31:01 +0200

    xd

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wciskaj = new System.Windows.Forms.Button();
+            this.ln = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ln)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wciskaj
+            // 
+            this.wciskaj.Location = new System.Drawing.Point(360, 258);
+            this.wciskaj.Name = "wciskaj";
+            this.wciskaj.Size = new System.Drawing.Size(75, 23);
+            this.wciskaj.TabIndex = 0;
+            this.wciskaj.Text = "wciskaj";
+            this.wciskaj.UseVisualStyleBackColor = true;
+            this.wciskaj.Click += new System.EventHandler(this.wciskaj_Click);
+            // 
+            // ln
+            // 
+            this.ln.Location = new System.Drawing.Point(289, 134);
+            this.ln.Name = "ln";
+            this.ln.Size = new System.Drawing.Size(120, 23);
+            this.ln.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ln);
+            this.Controls.Add(this.wciskaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ln)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button wciskaj;
+        private NumericUpDown ln;
+    }
+}
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         /*
+        private Dictionary<int, int> cache = new Dictionary<int, int>()
+        {
+            [0] = 0,
+            [1] = 1
+        };
+        */
+        private void wciskaj_Click(object sender, EventArgs e)
+        {
+            long liczba = (long)ln.Value;
+            long wyn = fib2(liczba);
+            MessageBox.Show(wyn.ToString());
+        }
+        
+        long fib2(long n)
+        {
+            if (n == 0 || n == 1) return n;
+            long[] wyrazy = new long[n+1];
+            wyrazy[1] = 1;
+            
+
+            for(int i = 2; i < wyrazy.Length; i++)
+            {
+                wyrazy[i] = wyrazy[i - 1] + wyrazy[i - 2];
+            }
+            // robimy tutaj jeszcze raz t¹ operacjê bo pêtla siê koñczy przed pi¹tym elementem którego chcemy wyliczyæ i to zwracamy, jeœli nie jest n+1 w new.
+            //            return wyrazy[n - 1] + wyrazy[n - 2
+            return wyrazy[n];
+        }
+        
+    }
+}
