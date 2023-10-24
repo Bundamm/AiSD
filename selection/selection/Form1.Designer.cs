64416b08bdf5a18c3c2dfaf5da3ec2475d5b53f2
bundamm <tomcio.szewa@gmail.com>
2023-10-24 17:07:53 +0200

    selection sort wykonany

---
+++ selection/selection/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace selection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(368, 206);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ selection/selection/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace selection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 3, 5, 7, 2, 6, 8, 1, 10 };
+            string res = selSort(tab);
+            MessageBox.Show(res);
+
+            
+            
+
+            
+        }
+        string selSort(int[] tab)
+        {
+            int minI,temp;
+            for (int i = 0; i < tab.Length-1; i++)
+            {
+                minI = i;
+                for (int j = i; j < tab.Length; j++) //i+1?
+                {
+                    if (tab[j] < tab[minI])
+                    {
+                        minI = j;
+                    }
+                }
+                temp = tab[i];
+                tab[i] = tab[minI];
+                tab[minI] = temp;
+            }
+            string wynik = "";
+            for(int i = 0; i < tab.Length; i++) {
+                wynik += tab[i].ToString() + ", ";
+            }
+            return wynik;
+        }
+    }
+}
