8aa7d1a1359f8e0c116d23dce890e87b27502049
bundamm <tomcio.szewa@gmail.com>
2023-11-07 18:54:29 +0100

    ok

---
+++ select/select/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace select
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            start = new Button();
+            tbInput = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // start
+            // 
+            start.Location = new Point(345, 232);
+            start.Name = "start";
+            start.Size = new Size(75, 23);
+            start.TabIndex = 0;
+            start.Text = "Start";
+            start.UseVisualStyleBackColor = true;
+            start.Click += start_Click;
+            // 
+            // tbInput
+            // 
+            tbInput.Location = new Point(345, 50);
+            tbInput.Name = "tbInput";
+            tbInput.Size = new Size(100, 23);
+            tbInput.TabIndex = 1;
+            tbInput.TextChanged += tbInput_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(364, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 364);
+            Controls.Add(label1);
+            Controls.Add(tbInput);
+            Controls.Add(start);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button start;
+        private TextBox tbInput;
+        private Label label1;
+    }
+}
---
+++ select/select/Form1.cs
@@ -0,0 +1,68 @@
+namespace select
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            string tabs = tbInput.Text;
+            int[] tab = convertInt(tabs);
+            int[] tab2 = selectSort(tab);
+            string wyn = convertString(tab2);
+            label1.Text = ("Wyn: " + wyn);
+        }
+        int[] convertInt(string napis)
+        {
+            var liczbyS = napis.Trim().Split(' ');
+            int[] liczby = new int[liczbyS.Length];
+            for (int i = 0; i < liczbyS.Length; i++)
+            {
+                liczby[i] = int.Parse(liczbyS[i]);
+            }
+            return liczby;
+        }
+
+        string convertString(int[] tab)
+        {
+            string wynik = "";
+            for (int i = 0; i < tab.Length; i++)
+            {
+                wynik += tab[i].ToString() + " ";
+            }
+            return wynik;
+        }
+        int[] selectSort(int[] tab)
+        {
+            int minI, temp;
+            for (int i = 0; i < tab.Length - 1; i++)
+            {
+                minI = i;
+                for (int j = i; j < tab.Length; j++)
+                {
+                    if (tab[j] < tab[minI])
+                    {
+                        minI = j;
+                    }
+                }
+                temp = tab[i];
+                tab[i] = tab[minI];
+                tab[minI] = temp;
+            }
+            return tab;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
