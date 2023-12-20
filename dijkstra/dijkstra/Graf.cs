9875c4e7976bc389b17ff0c2c42ff08e12eb1fc5
Bundamm <94806624+Bundamm@users.noreply.github.com>
2023-12-20 09:27:24 +0100

    bozeee

--- dijkstra/dijkstra/Graf.cs
+++ dijkstra/dijkstra/Graf.cs
@@ -9,11 +9,10 @@ namespace dijkstra
     public class Wezel4
     {
         public int wartosc;
-        public int iloscKrawedzi;
+        public List<Krawedz> ListaKrawedzi = new List<Krawedz>();
         public Wezel4(int wartosc)
         {
             this.wartosc = wartosc;
-            this.iloscKrawedzi = 0;
         }
     }
     public class Krawedz
@@ -27,27 +26,39 @@ namespace dijkstra
             this.waga = waga;
             this.poczatek = poczatek;
             this.koniec = koniec;
-            poczatek.iloscKrawedzi++;
+
         }
     }
-    
+    // var x = this.listaWezlow.ToList(); kopiowanie listy
+
+    public class Element
+    {
+        public int dystans;
+        public Wezel4 poprzednik;
+        public Element(int dystans, Wezel4 poprzednik)
+        {
+            this.dystans = dystans;
+            this.poprzednik = poprzednik;
+        }
+    }
+
     public class Graf
     {
-        public List<Wezel4> wezly = new List<Wezel4>();
+        public List<Wezel4> Q = new List<Wezel4>();
         public List<Krawedz> krawedzie = new List<Krawedz>();
         public Graf(Wezel4 w)
         {
-            wezly.Add(w);
+            Q.Add(w);
         }
         public void Add(Wezel4 w, Wezel4 k, int waga)
         {
-            if(!wezly.Contains(w))
+            if(!Q.Contains(w))
             {
-                wezly.Add(w);
+                Q.Add(w);
             }
-            if(!wezly.Contains(k))
+            if(!Q.Contains(k))
             {
-                wezly.Add(k);
+                Q.Add(k);
             }
             var temp = new Krawedz(waga, w, k);
             if (!krawedzie.Contains(temp))
@@ -56,40 +67,27 @@ namespace dijkstra
             }
         }
 
+
+        public Wezel4 ZnajdzMin(Dictionary<Wezel4> tabelka, List<Wezel4> Q)
+        {
+
+        }
+
         public void Dijkstra(Wezel4 start)
         {
             List<int> odleglosci = new List<int>(wezly.Count);
             List<Wezel4> poprzednicy = new List<Wezel4>(wezly.Count);
-            List<Wezel4> nieodwiedzone = new List<Wezel4>(wezly);
-
-            foreach(var w in wezly)
-            {
-                odleglosci.Add(int.MaxValue);
-                poprzednicy.Add(null);
-            }
-            odleglosci[wezly.IndexOf(start)] = 0;
             
-            while(nieodwiedzone.Count > 0)
+            odleglosci[wezly.IndexOf(start)] = 0;
+            poprzednicy[wezly.IndexOf(start)] = null;
+            foreach (var w in wezly)
             {
-                Wezel4 aktualny = null;
-                foreach(var w in nieodwiedzone)
-                {
-                    if(aktualny == null || odleglosci[wezly.IndexOf(w)] < odleglosci[wezly.IndexOf(aktualny)])
-                    {
-                        aktualny = w;
-                    }
-                }
-                nieodwiedzone.Remove(aktualny);
-                foreach (var krawedz in krawedzie)
-                {
-                    int nowaOdleglosc = odleglosci[wezly.IndexOf(aktualny)] + krawedz.waga;
-                    if(nowaOdleglosc < odleglosci[wezly.IndexOf(krawedz.koniec)])
-                    {
-                        odleglosci[wezly.IndexOf(krawedz.koniec)] = nowaOdleglosc;
-                        poprzednicy[wezly.IndexOf(krawedz.koniec)] = aktualny;
-                    }
-                }
+                odleglosci[wezly.IndexOf(w)] = int.MaxValue;
+                poprzednicy[wezly.IndexOf(w)] = null;
             }
+            
+            
+            
             
 
         }
